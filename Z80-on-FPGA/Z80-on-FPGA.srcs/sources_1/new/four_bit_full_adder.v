ca62907cf2ebf10a830586507fc9d40ad4827100
unknown <luc.gaborit@gmail.com>
2023-01-08 14:55:34 +0100

    Added all the inputs and changed their names

--- Z80-on-FPGA/Z80-on-FPGA.srcs/sources_1/new/four_bit_full_adder.v
+++ Z80-on-FPGA/Z80-on-FPGA.srcs/sources_1/new/four_bit_full_adder.v
@@ -1,8 +1,11 @@
 // 4-bit full adder
 module four_bit_full_adder(
-    input [3:0] data_in,
+    input [3:0] A,
+    input [3:0] B,
     input Cin,
-    output [3:0] data_out,
+    output [3:0] S,
     output Cout
-    );
+);
+
+
 endmodule
