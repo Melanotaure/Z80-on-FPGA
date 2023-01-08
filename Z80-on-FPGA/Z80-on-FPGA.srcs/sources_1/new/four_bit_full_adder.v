924f4e620ccaf957f5b5eefe8a52097ace224734
unknown <luc.gaborit@gmail.com>
2023-01-08 15:48:11 +0100

    Refacto

--- Z80-on-FPGA/Z80-on-FPGA.srcs/sources_1/new/four_bit_full_adder.v
+++ Z80-on-FPGA/Z80-on-FPGA.srcs/sources_1/new/four_bit_full_adder.v
@@ -1,10 +1,10 @@
 // 4-bit full adder
 module four_bit_full_adder(
-    input [3:0] A,
-    input [3:0] B,
-    input Cin,
-    output [3:0] S,
-    output Cout
+    input   [3:0]   A,
+    input   [3:0]   B,
+    input           Cin,
+    output  [3:0]   S,
+    output          Cout
 );
 
 assign {Cout, S} = A + B + Cin;
